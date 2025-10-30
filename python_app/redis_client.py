4c8d650be8c606d2351ec56aa77d6ab3b129b4ff
Akhil Kakkireni <akhilkakkireni15@gmail.com>
2025-10-30 08:12:21 +0000

    owasp

--- python_app/redis_client.py
+++ python_app/redis_client.py
@@ -4,7 +4,7 @@ def get_redis_client():
     # ❌ Intentionally hardcoded secret
     redis_host = "localhost"
     redis_port = 6379
-    redis_password = "Password123!"  # Sonar will flag this
+   
     
     try:
         client = redis.Redis(
